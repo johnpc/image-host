d821ef804d6e1599b72cfb56aaf9595cd12f1a46
John Corser <johnpc@umich.edu>
2024-03-19 19:58:03 -0400

    add headers

--- src/app/images/[key]/route.ts
+++ src/app/images/[key]/route.ts
@@ -2,6 +2,7 @@ export const dynamic = "force-dynamic";
 import { Amplify } from "aws-amplify";
 import config from "../../../../amplifyconfiguration.json";
 import { downloadData } from "aws-amplify/storage";
+import { NextResponse } from "next/server";
 Amplify.configure(config);
 
 export async function GET(
@@ -15,5 +16,16 @@ export async function GET(
     },
   }).result;
   const blob = await body.blob();
-  return new Response(blob);
+
+  const response = new NextResponse(blob, {
+    status: 200,
+    headers: [
+      ["Content-Type", "image/png"],
+      ["Accept-Ranges", "bytes"],
+      ["Cache-Control", "public, max-age=0"],
+      ["Content-Length", `${blob.size}`],
+    ],
+  });
+
+  return response;
 }
