c719023db3b6c0495afcf3007df8df9e465b4235
John Corser <johnpc@umich.edu>
2024-03-22 12:32:41 -0400

    use stream

--- src/app/d/[key]/route.ts
+++ src/app/d/[key]/route.ts
@@ -1,31 +1,51 @@
 export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
+import { contentType } from "mime-types";
 import { Amplify } from "aws-amplify";
 import config from "../../../../amplifyconfiguration.json";
-import { downloadData } from "aws-amplify/storage";
+import { downloadData, getProperties } from "aws-amplify/storage";
 Amplify.configure(config);
 
 export async function GET(
   req: Request,
   { params }: { params: { key: string } },
 ) {
+  const properties = await getProperties({ key: params.key });
+  console.log({ properties });
+
   const { body } = await downloadData({
     key: params.key as string,
     options: {
       accessLevel: "guest",
+      onProgress: ({ transferredBytes, totalBytes }) => {
+        console.log({
+          transferredBytes,
+          totalBytes,
+        });
+      },
     },
   }).result;
   const blob = await body.blob();
+  const stream = await blob.stream();
+  const contentTypeString =
+    contentType(params.key) || "application/octet-stream";
+  const headers: [string, string][] = [
+    ["Content-Type", properties.contentType ?? contentTypeString],
+    ["Accept-Ranges", "bytes"],
+    ["Cache-Control", "public, max-age=0"],
+    ["Content-Length", `${properties.size}`],
+  ];
 
-  const response = new NextResponse(blob, {
+  if (contentTypeString.includes("octet-stream")) {
+    headers.push([
+      "Content-Disposition",
+      `attachment; filename="${params.key}"`,
+    ]);
+  }
+
+  const response = new NextResponse(stream, {
     status: 200,
-    headers: [
-      ["Content-Type", "application/octet-stream"],
-      ["Content-Disposition", `attachment; filename="${params.key}"`],
-      ["Accept-Ranges", "bytes"],
-      ["Cache-Control", "public, max-age=0"],
-      ["Content-Length", `${blob.size}`],
-    ],
+    headers,
   });
 
   return response;
